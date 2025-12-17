1a03a6e6a3c41145498c5060c7aa2b01df6a9ffc
yagizsenn <yagizefesen5@gmail.com>
2025-12-17 12:09:10 +0300

    Implemented fifth methods of my project

--- Main.java
+++ Main.java
@@ -134,8 +134,30 @@ public class Main {
         return bestDay;
     }
     
-    public static String bestMonthForCommodity(String comm) { 
-        return "DUMMY"; 
+    public static String bestMonthForCommodity(String comm) {
+        int cIndex = -1;
+        for (int c = 0; c < COMMS; c++) {
+            if (commodities[c].equals(comm)) {
+                cIndex = c;
+                break;
+            }
+        }
+        if (cIndex == -1) return "INVALID_COMMODITY";
+
+        int bestMonth = 0;
+        int bestSum = Integer.MIN_VALUE;
+
+        for (int m = 0; m < MONTHS; m++) {
+            int sum = 0;
+            for (int d = 0; d < DAYS; d++) {
+                sum += Data[m][d][cIndex];
+            }
+            if (sum > bestSum) {
+                bestSum = sum;
+                bestMonth = m;
+            }
+        }
+        return months[bestMonth];
     }
 
     public static int consecutiveLossDays(String comm) { 
